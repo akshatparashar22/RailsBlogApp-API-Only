8ae9eef88e412b9fc28a4b609a59b497a056f44c
akshatparashar22 <akshatparashar2001@gmail.com>
2024-09-23 21:23:50 +0530

    Initial commit

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::ArticlesController < ApplicationController
+    # before_action :authenticate_user!, except: [:index, :show]
+  def index
+    @articles = Article.all
+    render json: @articles
+end
+def show
+    @article = Article.find(params[:id])
+    render json: @article
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+        render json: @article, status: :updated
+    else
+        render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    
+    @article.destroy
+
+    head :no_content
+  end
+
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body, :status)
+    end
+end
